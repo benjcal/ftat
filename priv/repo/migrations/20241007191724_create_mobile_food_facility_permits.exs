693aa57bcae6f78c6f9dafeeaf2dfca1f410c1ab
Benjamin Calderon <benj.calderon@gmail.com>
2024-10-07 15:29:38 -0400

    fix size

--- priv/repo/migrations/20241007191724_create_mobile_food_facility_permits.exs
+++ priv/repo/migrations/20241007191724_create_mobile_food_facility_permits.exs
@@ -9,7 +9,7 @@ defmodule Ftat.Repo.Migrations.CreateMobileFoodFacilityPermits do
       add :blocklot, :string
       add :permit, :string
       add :permit_status, :string
-      add :food_items, :string
+      add :food_items, :string, size: 4092
       add :permit_approved_date, :date
       add :permit_expiration_date, :date
       add :prior_permit, :boolean, default: false, null: false
